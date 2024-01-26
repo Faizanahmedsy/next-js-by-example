ef5de0788287a4ae84b3d29a9aa9295a1f6be12c
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-26 14:47:30 +0530

    added notes

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>I am Footer I will be seen on every page</footer>;
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <nav>I am a navbar I will be seen in all pages </nav>;
+};
+
+export default Navbar;
